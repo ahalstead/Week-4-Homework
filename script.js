653fe1a455b782ef8460de396b22cc0665d504e5
Alex Halstead <ahalstead@gmail.com>
2020-04-17 22:20:42 -0600

    Code updates with working prompts

--- script.js
+++ script.js
@@ -1,116 +1,119 @@
-// Assignment Code
+// Array with options Lower, upper, numeric, special
 var generateBtn = document.querySelector("#generate");
-
-
-function generatePassword(){
+var passwordTextBox = document.querySelector("#passwordtextbox")
 
 var lowercaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
 var uppercaseArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
 var numericArray = ["0","1","2","3","4","5","6","7","8","9"];
+
 var specialCharactersArray = ["?","=",".","*","[","!","@","#","$","%","^","&","]","(",")"];
 
-// While loops to prompt users to input for password, lowercase, numeric, 
+var password = "";
 
+var passwordOptions = {
+  lowercase: false,
+  uppercase: false,
+  numeric: false,
+  specialCharacters: false,
+}
+
+//Establish function to prompt user for pass
+function passwordNeeds() {
   var passwordInput = prompt('How long would you like the generated Password to be? (Must be between 8 and 128 characters).');
   while (passwordInput > 128 || passwordInput < 8) {  
      passwordInput = prompt('How long would you like the generated Password to be? (Must be between 8 and 128 characters.');
   }
-  var lowercase = prompt('Would you like to include lowercase character(s)? Yes or No?');
-  while (!(lowercase == "Yes" || lowercase == "No")) {
-    lowercase = prompt('Would you like to include lowercase character(s)? Yes or No?')
-  }
-  var numeric = prompt('numeric');
-  while (!(numeric == "Yes" || numeric == "No")) {
-    numeric = prompt('Would you like to include numeric character(s)? Yes or No?')
-  }
-  var specialCharacters = prompt('special characters');
-  while (!(specialCharacters == "Yes" || specialCharacters == "No")) {
-    specialCharacters = prompt('Would you like to include special character(s)? Yes or No?')
-  }
-  var uppercase = prompt('uppercase');
-  while (!(uppercase == "Yes" || uppercase == "No")) {
-    uppercase = prompt('Would you like to include uppercase character(s)? Yes or No?')
-}
+console.log("passwordNeeds")
+  
 
-
-
-// Write password to the #password input
-var password = generatePassword();
-
-function writePassword() {
-  generatePassword()
-  passwordMin();
-  passwordMax(minimumLength);
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-var passwordLengthMin = passwordMin();
-
-//Establish function to prompt user for maximum password length
-
-function passwordMax(minimumLength) {
-
-  minimumLength = parseInt(minimumLength);
-
-  var maximumLength = prompt("What is the maximum length of your desired password?");
-  maximumLength = parseInt(maximumLength);
-  if (maximumLength > 128) {
-    
-    alert("Your password cannot exceed 128 characters in length.");
-   
-    passwordMax(minimumLength);
-  }
-  else if (maximumLength < minimumLength) {
-    
-    alert("Your password's maximum length must at least be equal to its minimum length.");
-    
-    passwordMax(minimumLength);
-  }
-  return maximumLength;
-}
-console.log(password);
-
-//Establish variable for maximum pass
-var passwordLengthMax = passwordMax(passwordLengthMin);
-
-//Establish function to prompt user for pass
-function passwordNeeds() {
-  var selections = "";
   var lowercase = prompt("Does your password need to include lowercase letters? (y/n)").toLowerCase();    
   if (lowercase === "y") {
-    selections = selections + " " + "lowercase";
+  passwordOptions.lowercase = true
+
   }
+
   var uppercase = prompt("Does your password need to include uppercase letters? (y/n)").toLowerCase();    
   if (uppercase === "y") {
-    selections = selections + " " + "uppercase";
+    passwordOptions.uppercase = true
   }
+
   var numeric = prompt("Does your password need to include numeric values? (y/n)").toLowerCase();    
   if (numeric === "y") {
-    selections = selections + " " + "numeric";
+    passwordOptions.numeric = true
   }
+
   var specialCharacters = prompt("Does your password need to include special characters? (y/n)").toLowerCase();
   if (specialCharacters === "y") {
-    selections = selections + " " + "special characters";
+    passwordOptions.specialCharacters = true
   }
+
   if (lowercase === "n" && uppercase === "n" && numeric === "n" && specialCharacters === "n") 
   {
+    console.log("if")
     alert("Your password must contain at least one of the character types (lowercase, uppercase, numeric, or special characters)");
     passwordNeeds();
   
+ 
+ 
   }
-  else {
-    alert("`You have selected the following criteria:" + selections);
-    var answer = prompt("Would you like to continue?");
-    answer = answer.toLowerCase();
-    if (answer === "n") {
-      passwordNeeds();
-    }
-  }
-
-  generateBtn.addEventListener("click", writePassword);  
-  }
-  
+else 
+{
+  console.log("else")
+generatePassword(passwordInput);
 }
 
+}
+
+function generatePassword(length) {
+  var finalPassword = "";
+console.log("generatePassword")
+  //for (var i = 0;) i > 4; 
+  for (var i = 0; i < length; i++){
+    console.log (passwordOptions.lowercase)
+    if (passwordOptions.lowercase === true) {
+     var char = getRandom (lowercaseArray);
+console.log(char)
+ //This will combine var finalpassword    
+  finalPassward = finalPassword + char; 
+    }
+    if (passwordOptions.uppercase === true){
+     var char = getRandom (uppercaseArray);
+     console.log("char", char)
+     finalPassward = finalPassword + char; 
+    }
+    if (passwordOptions.numeric === true){
+     var char = getRandom (numericArray);
+     finalPassward = finalPassword + char; 
+    }
+    if (passwordOptions.specialCharactersArray){
+     var char = getRandom (specialCharctersArray);
+     finalPassward = finalPassword + char; 
+    }
+    console.log(typeof finalPassword)
+  }
+  password = finalPassword;
+// 1. Set a password variable to an empty string
+// 2. Loop over length, e.g. 10 times
+// 3. For every option selected, need to insert that option into the password from the arrays at the top
+// 4. Show the password on the HTML page, after the loop has finished. 
+}
+
+function getRandom(arr){
+var getRandomIndex = Math.floor(Math.random()*arr.length);
+var char = arr[getRandomIndex];
+return char;
+}
+console.log(getRandom)
+
+
+ // 1. Get a random index from the array. Math.floor, math.random (based on the length of the array) 
+ // 2. Get a char, from the array based on a random index
+ // 3. Return a char. On line 67 it will be assigned (for exam)
+
+
+
+//Generate the finalpassword:
+passwordTextBox.value = password;
+generateBtn.addEventListener("click", passwordNeeds); 
